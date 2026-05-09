46684033bf7198547a9433fc9984d9515c50d0c2
Aliqulov03 <aliqulovazizjon68@gmail.com>
2026-05-09 12:18:20 +0500

    fix: correct Redis import and add type annotation for error handling

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,12 +1,12 @@
-import IORedis from 'ioredis';
+import { Redis } from 'ioredis';
 import { env } from '../config/env.js';
 import { logger } from '../utils/logger.js';
 
-export const redis = new IORedis(env.REDIS_URL, {
+export const redis = new Redis(env.REDIS_URL, {
   maxRetriesPerRequest: null,
   enableReadyCheck: true,
   lazyConnect: false,
 });
 
 redis.on('connect', () => logger.info('Redis connected'));
-redis.on('error', (err) => logger.error({ err }, 'Redis error'));
+redis.on('error', (err: Error) => logger.error({ err }, 'Redis error'));
